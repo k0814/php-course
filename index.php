d748fcf4d9aaa367f6a281ae33cb2410464c0d88
King_Kobia <kobiacaa@gmail.com>
2020-11-04 21:34:11 +0300

    add foreach loop

--- index.php
+++ index.php
@@ -13,6 +13,15 @@ PHP Store
 
     echo "<h1>Welcome to ".$name." </h1>";
     echo "<h2>You have ".$credit." in your wallet!</h2>";
+
+        $products['computer']=750;
+        $products['Car']=15000;
+        $products['iPhone']=1000;
+        $products['Toaster']=75;
+    echo "<p>A car costs $".$products['Car']."</p>";
+    foreach($products as $key => $value){
+        echo "<p>The value of a ".$key." is ".$value."!!</p>";
+    }
     ?>
 </body>
 </html>
