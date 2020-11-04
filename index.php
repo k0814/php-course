14bfd39d58ebb0b77a6d29349d2529ebe92e4f1f
King_Kobia <kobiacaa@gmail.com>
2020-11-04 21:08:52 +0300

    add variables

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ PHP Store
     $name="PHP store";
     $credit=1000;
 
-    echo "<h1>Welcome to ".$name"!</h1>";
-    echo "<h2>You have ".$credit" in your wallet!</h2>";
+    echo "<h1>Welcome to ".$name." </h1>";
+    echo "<h2>You have ".$credit." in your wallet!</h2>";
     ?>
 </body>
 </html>
